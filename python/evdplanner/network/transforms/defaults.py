671ff5a0008a72db7cbfe63a6c7a5f1b8c89d751
Mathijs de Boer <m.deboer-41@umcutrecht.nl>
2024-02-28 18:16:22 +0100

    Allow removing file reading from default transforms

--- python/evdplanner/network/transforms/defaults.py
+++ python/evdplanner/network/transforms/defaults.py
@@ -14,7 +14,9 @@ def default_load_transforms(
     json_key: str = "keypoints",
     image_key: str = "image",
     label_key: str = "label",
+    include_file_reading: bool = True,
     allow_missing_keys: bool = False,
+    input_channel_dim: int | None = None,
 ) -> list[mt.Transform]:
     """
     Default transforms for loading and preprocessing data.
@@ -31,29 +33,39 @@ def default_load_transforms(
         The key for the image.
     label_key : str, optional
         The key for the label.
+    include_file_reading : bool, optional
+        Whether to include file reading or not.
     allow_missing_keys : bool, optional
         Whether to allow missing keys or not.
+    input_channel_dim : int, optional
+        The input channel dimension.
 
     Returns
     -------
     list[mt.Transform]
         The list of transforms.
     """
-    return [
-        mt.LoadImaged(keys=maps),
+    transforms = [
         mt.EnsureTyped(keys=maps, dtype=torch.float32),
-        JsonKeypointLoaderd(
-            json_key=json_key,
-            output_key=label_key,
-            keypoint_names=keypoints,
-            allow_missing_keys=allow_missing_keys,
-        ),
-        mt.EnsureChannelFirstd(keys=maps),
+        mt.EnsureChannelFirstd(keys=maps, channel_dim=input_channel_dim),
         mt.ScaleIntensityd(keys=maps, minv=-1.0, maxv=1.0, channel_wise=False),
         mt.ConcatItemsd(keys=maps, name=image_key, dim=0),
         mt.DeleteItemsd(keys=[*maps, json_key]),
         mt.ToTensord(keys=[image_key, label_key], allow_missing_keys=allow_missing_keys),
     ]
+
+    if include_file_reading:
+        transforms = [
+            mt.LoadImaged(keys=maps),
+            JsonKeypointLoaderd(
+                json_key=json_key,
+                output_key=label_key,
+                keypoint_names=keypoints,
+                allow_missing_keys=allow_missing_keys,
+            ),
+        ] + transforms
+
+    return transforms
 
 
 def default_augment_transforms(
