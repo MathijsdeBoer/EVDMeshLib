d7181719d1d674e112135d10d630b50ba1c7e133
Mathijs de Boer <m.deboer-41@umcutrecht.nl>
2024-03-11 13:27:10 +0100

    Move resolution parameter to config file
    Signed-off-by: Mathijs de Boer <m.deboer-41@umcutrecht.nl>

--- python/evdplanner/cli/model/train/__init__.py
+++ python/evdplanner/cli/model/train/__init__.py
@@ -92,12 +92,6 @@ import click
     help="Random seed for reproducibility.",
 )
 @click.option(
-    "--resolution",
-    type=int,
-    default=1024,
-    help="Resolution of input images.",
-)
-@click.option(
     "--num-workers",
     type=int,
     default=0,
@@ -113,6 +107,11 @@ import click
     "--plot-progress",
     is_flag=True,
     help="Plot progress to file and TensorBoard during training.",
+)
+@click.option(
+    "--use-maps",
+    is_flag=True,
+    help="Use pre-rendered maps during training.",
 )
 @click.option(
     "--train-augment",
@@ -139,10 +138,10 @@ def train(
     val_root: Path | None = None,
     test_root: Path | None = None,
     seed: int | None = None,
-    resolution: int = 1024,
     num_workers: int = 0,
     verbose: int = 0,
     plot_progress: bool = False,
+    use_maps: bool = False,
     use_augmentations: bool = False,
     final_bias: Path | None = None,
 ) -> None:
@@ -164,21 +163,21 @@ def train(
     epochs : int
         Number of epochs to train.
     augment_root : Path, optional
-        Root directory containing augmented training data.
+        Root directory containing pre-augmented training data.
     val_root : Path, optional
         Root directory containing validation data.
     test_root : Path, optional
         Root directory containing test data.
     seed : int, optional
         Random seed for reproducibility.
-    resolution : int, optional
-        Resolution of input images.
     num_workers : int, optional
         Number of workers for data loading.
     verbose : int, optional
         Increase verbosity. (Use multiple times for more verbosity.)
     plot_progress : bool, optional
         Plot progress to file and TensorBoard during training.
+    use_maps : bool, optional
+        Use pre-rendered maps during training.
     use_augmentations : bool, optional
         Use augmentations during training.
     final_bias : Path, optional
@@ -216,6 +215,7 @@ def train(
     from evdplanner.network.transforms.defaults import (
         default_augment_transforms,
         default_load_transforms,
+        default_mesh_load_transforms,
     )
 
     set_verbosity(verbose)
@@ -230,22 +230,38 @@ def train(
     if not model_path.parent.exists():
         model_path.parent.mkdir(parents=True)
 
+    with config.open("r") as file:
+        logger.info(f"Loading configuration from {config}.")
+        config = json.load(file)
+
+    if "resolution" not in config:
+        logger.warning("Resolution not found in configuration. Using default of 1024.")
+        resolution = 1024
+    else:
+        resolution = config["resolution"]
+
     logger.info(f"Collecting data from {train_root}.")
-    train_samples, maps, keypoints = get_data(
+    train_samples, maps, label_names = get_data(
         train_root,
         anatomy,
-        output_label_key="keypoints",
+        use_maps=use_maps,
         resolution=resolution,
     )
 
+    logger.debug(f"Found {len(train_samples)} samples.")
+    logger.debug(f"Using maps: {maps}")
+    logger.debug(f"Using keypoints: {label_names}")
+
     if augment_root:
         logger.info(f"Collecting data from {augment_root}.")
-        augment_samples, _, _ = get_data(augment_root, anatomy, resolution=resolution)
+        augment_samples, _, _ = get_data(
+            augment_root, anatomy, use_maps=use_maps, resolution=resolution
+        )
         train_samples += augment_samples
 
     if val_root:
         logger.info(f"Collecting data from {val_root}.")
-        val_samples, _, _ = get_data(val_root, anatomy, resolution=resolution)
+        val_samples, _, _ = get_data(val_root, anatomy, use_maps=use_maps, resolution=resolution)
     else:
         logger.warning("No validation data provided. Using 20% of training data for validation.")
         val_samples = sample(train_samples, int(0.2 * len(train_samples)))
@@ -253,7 +269,7 @@ def train(
 
     if test_root:
         logger.info(f"Collecting data from {test_root}.")
-        test_samples, _, _ = get_data(test_root, anatomy, resolution=resolution)
+        test_samples, _, _ = get_data(test_root, anatomy, use_maps=use_maps, resolution=resolution)
     else:
         logger.info("No test data provided. Using validation data for testing.")
         test_samples = None
@@ -264,7 +280,7 @@ def train(
             final_bias = json.load(file)
 
         bias = []
-        for key in keypoints:
+        for key in label_names:
             for b in final_bias:
                 if b["label"] == key:
                     bias.append(b["position"])
@@ -274,85 +290,85 @@ def train(
         logger.warning(f"File {final_bias} does not exist. Ignoring.")
         final_bias = None
 
-    with config.open("r") as file:
-        logger.info(f"Loading configuration from {config}.")
-        config = json.load(file)
+    logger.debug("Configuration:")
+    logger.debug(json.dumps(config, indent=4))
 
-        logger.debug("Configuration:")
-        logger.debug(json.dumps(config, indent=4))
+    if anatomy == "skin":
+        logger.debug("Setting ranges for skin.")
+        x_resolution = resolution
+        y_resolution = resolution // 2
+        x_range = (0.0, 2.0 * pi)
+        y_range = (0.0, pi)
+    elif anatomy == "ventricles":
+        logger.debug("Setting ranges for ventricles.")
+        x_resolution = resolution
+        y_resolution = resolution
+        x_range = (0.0, 1.0)
+        y_range = (0.0, 1.0)
+    else:
+        msg = f"Anatomy '{anatomy}' not recognized."
+        logger.error(msg)
+        raise ValueError(msg)
 
-        dm = EVDPlannerDataModule(
-            train_samples=train_samples,
-            val_samples=val_samples,
-            maps=maps,
-            keypoints_key="keypoints",
-            test_samples=test_samples,
-            load_transforms=default_load_transforms(maps, keypoints),
-            augment_transforms=default_augment_transforms() if use_augmentations else None,
-            batch_size=config["batch_size"],
-            num_workers=num_workers,
+    dm = EVDPlannerDataModule(
+        train_samples=train_samples,
+        val_samples=val_samples,
+        test_samples=test_samples,
+        load_transforms=default_load_transforms(maps, label_names)
+        if use_maps
+        else default_mesh_load_transforms(
+            x_resolution=x_resolution,
+            y_resolution=y_resolution,
+            include_augmentations=use_augmentations,
+        ),
+        augment_transforms=default_augment_transforms() if use_augmentations else None,
+        batch_size=config.get("batch_size", 1),
+        num_workers=num_workers,
+    )
+
+    metrics = [
+        MAEMetric(),
+        MSEMetric(),
+        MeanSquaredAngularError(x_range=x_range, y_range=y_range),
+        MeanAbsoluteAngularError(x_range=x_range, y_range=y_range),
+    ]
+    logger.debug(f"Using metrics: {metrics}")
+
+    core_model = PointRegressor.from_optuna_parameters(
+        config,
+        maps=maps,
+        keypoints=label_names,
+        in_shape=(4, x_resolution, y_resolution),
+        out_shape=(len(label_names), 2),
+        final_bias=final_bias,
+    )
+
+    optimizer = get_optimizer(
+        config["optimizer"], core_model.parameters(), **config["optimizer_args"]
+    )
+    if "scheduler" in config:
+        scheduler = get_lr_scheduler(
+            config.get("scheduler", None),
+            optimizer,
+            epochs=epochs,
+            **config["scheduler_args"],
         )
+    else:
+        scheduler = None
 
-        if anatomy == "skin":
-            logger.debug("Setting ranges for skin.")
-            x_resolution = resolution
-            y_resolution = resolution // 2
-            x_range = (0.0, 2.0 * pi)
-            y_range = (0.0, pi)
-        elif anatomy == "ventricles":
-            logger.debug("Setting ranges for ventricles.")
-            x_resolution = resolution
-            y_resolution = resolution
-            x_range = (0.0, 1.0)
-            y_range = (0.0, 1.0)
-        else:
-            msg = f"Anatomy '{anatomy}' not recognized."
-            logger.error(msg)
-            raise ValueError(msg)
+    logger.debug("Creating model.")
+    model = LightningWrapper.build_wrapper(
+        model=core_model,
+        loss=get_loss_fn(config["loss_fn"]),
+        optimizer=optimizer,
+        scheduler=scheduler,
+        metrics=metrics,
+        config=config,
+    )
+    logger.debug(f"Setting input shape to (1, 4, {x_resolution}, {y_resolution}).")
+    model.set_input_shape((1, 4, x_resolution, y_resolution))
 
-        metrics = [
-            MAEMetric(),
-            MSEMetric(),
-            MeanSquaredAngularError(x_range=x_range, y_range=y_range),
-            MeanAbsoluteAngularError(x_range=x_range, y_range=y_range),
-        ]
-        logger.debug(f"Using metrics: {metrics}")
-
-        core_model = PointRegressor.from_optuna_parameters(
-            config,
-            maps=maps,
-            keypoints=keypoints,
-            in_shape=(4, x_resolution, y_resolution),
-            out_shape=(len(keypoints), 2),
-            final_bias=final_bias,
-        )
-
-        optimizer = get_optimizer(
-            config["optimizer"], core_model.parameters(), **config["optimizer_args"]
-        )
-        if "scheduler" in config:
-            scheduler = get_lr_scheduler(
-                config.get("scheduler", None),
-                optimizer,
-                epochs=epochs,
-                **config["scheduler_args"],
-            )
-        else:
-            scheduler = None
-
-        logger.debug("Creating model.")
-        model = LightningWrapper.build_wrapper(
-            model=core_model,
-            loss=get_loss_fn(config["loss_fn"]),
-            optimizer=optimizer,
-            scheduler=scheduler,
-            metrics=metrics,
-            config=config,
-        )
-        logger.debug(f"Setting input shape to (1, 4, {x_resolution}, {y_resolution}).")
-        model.set_input_shape((1, 4, x_resolution, y_resolution))
-
-        model.loggable_hparams = core_model.loggable_parameters()
+    model.loggable_hparams = core_model.loggable_parameters()
 
     logger.info("Training model.")
 
